cbf50b8995b62a317a5b78f4e77c059e6aafd67a
Miranda Michalski <mbrockhaus@gmail.com>
2016-03-13 14:06:46 -0400

    modified file1.js without adding readme.txt to loacal commit

--- file1.js
+++ file1.js
@@ -54,3 +54,45 @@ drwxr-xr-x 1 myTraining 197121 0 Mar 13 13:37 ../
 -rw-r--r-- 1 myTraining 197121 0 Mar 13 13:37 file1.js
 -rw-r--r-- 1 myTraining 197121 0 Mar 13 13:37 file2.js
 -rw-r--r-- 1 myTraining 197121 0 Mar 13 13:37 file3.js
+../day2warmup:
+total 2
+-rw-r--r-- 1 myTraining 197121 135 Mar 13 10:17 file1.js
+-rw-r--r-- 1 myTraining 197121 133 Mar 13 10:18 file2.js
+
+../exercise1:
+total 3
+-rw-r--r-- 1 myTraining 197121 59 Mar 12 13:45 andrew.txt
+-rw-r--r-- 1 myTraining 197121 76 Mar 12 14:21 bonnie.js
+-rw-r--r-- 1 myTraining 197121 88 Mar 12 14:22 daniel.js
+
+../exercise2:
+total 0
+-rw-r--r-- 1 myTraining 197121 0 Mar 13 10:27 file1.js
+-rw-r--r-- 1 myTraining 197121 0 Mar 13 10:27 file2.js
+
+../exercise3A:
+total 4
+-rw-r--r-- 1 myTraining 197121 749 Mar 13 11:01 newfile.js
+
+../exercise3B:
+total 12
+-rw-r--r-- 1 myTraining 197121 8652 Mar 13 13:00 fileB.js
+
+../gitclonedemo:
+total 0
+drwxr-xr-x 1 myTraining 197121 0 Mar 13 13:42 git-demo3/
+
+../git-cmd-demos:
+total 5
+-rw-r--r-- 1 myTraining 197121    0 Mar 13 10:31 file3.js
+-rw-r--r-- 1 myTraining 197121 1823 Mar 13 13:19 fileB.js
+-rw-r--r-- 1 myTraining 197121   80 Mar 12 14:28 helloWorld.js
+drwxr-xr-x 1 myTraining 197121    0 Mar 12 13:19 names/
+
+../mygdiaagittraining:
+total 10
+-rw-r--r-- 1 myTraining 197121 2133 Mar 13 13:37 file1.js
+-rw-r--r-- 1 myTraining 197121 1157 Mar 13 13:38 file12js
+-rw-r--r-- 1 myTraining 197121    0 Mar 13 13:37 file2.js
+-rw-r--r-- 1 myTraining 197121   52 Mar 13 13:38 file3.js
+-rw-r--r-- 1 myTraining 197121   65 Mar 13 14:05 README.md
